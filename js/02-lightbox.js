68d316f5bea268d9b0e63dbdc7bcfb1ac8d0ae23
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-11-14 18:32:27 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryImages = galleryItems.map(image => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+            />
+        </a>
+    </li>`
+)
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+const settingsSimpleLightbox = event => {
+    event.preventDefault();
+    new SimpleLightbox('.gallery__link',
+        {
+            captionsData: 'alt',
+            captionDelay: 250
+        }
+    );
+};
+
+gallery.addEventListener('click', settingsSimpleLightbox);
